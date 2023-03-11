9ad5dc2eab8e506b800c911761d8a8fa415469ae
Sita-Atchyutuni <76470052+Sita-Atchyutuni@users.noreply.github.com>
2023-03-11 15:54:05 -0600

    Update streamlit_app.py
    new header

--- streamlit_app.py
+++ streamlit_app.py
@@ -2,3 +2,4 @@ import streamlit
 streamlit.title('My parents new healthy diner')
 streamlit.header('🥣 🥗 🐔 🥑🍞Breakfast Menu')
 streamlit.text('Omega3')
+streamlit.header('🍌🥭 Build Your Own Fruit Smoothie 🥝🍇')
